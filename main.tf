0d0b567a8dc03d41de915f92f3b52ce9ce208903
Juan Camilo <juansoad.123@gmail.com>
2023-12-18 08:50:54 -0500

    testing ecs wout img default

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "aws_ecs_task_definition" "NewYorkMets_Task_Definition" {
   container_definitions = jsonencode([
     {
       Name  = "my-container"
-      image = "nginx:latest" 
+      image = "" 
       portMappings = [
         {
           containerPort = 80
